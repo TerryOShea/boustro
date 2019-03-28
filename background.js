dfd05ffb0471b6bd01b02767662aa65c02a5877f
Terry O'Shea <terrykoshea@gmail.com>
2019-03-28 11:55:16 -0400

    remove popup and fix toggling

--- background.js
+++ background.js
@@ -1,5 +1,29 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ active: true }, () => {
-        console.log("This is active");
-    });
+let hasBeenToggled = false;
+let isActive = false;
+
+chrome.browserAction.onClicked.addListener(tab => {
+    isActive = !isActive;
+
+    if (isActive) {
+        chrome.tabs.executeScript(tab.id, {
+            code: 'document.body.classList.add("boustro-active");',
+        });
+    } else {
+        chrome.tabs.executeScript(tab.id, {
+            code: 'document.body.classList.remove("boustro-active");',
+        });
+    }
+
+    if (!hasBeenToggled) {
+        hasBeenToggled = true;
+
+        chrome.tabs.insertCSS(tab.id, {
+            file: 'styles.css',
+        });
+
+        chrome.tabs.executeScript(tab.id, {
+            file: 'boustrophedize.js',
+        });
+    }
 });
+
